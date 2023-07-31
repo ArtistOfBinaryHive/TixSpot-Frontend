3928da07ae45b39b4e271494fe6c215fcd5ee713
haramishra <haraprasad45@gmail.com>
2023-07-31 21:12:46 +0530

    nav link components added

--- tixspot/src/components/list-links.tsx
+++ tixspot/src/components/list-links.tsx
@@ -1,10 +1,11 @@
 import * as React from "react"
-import Link from "next/link"
 
 import { NavItem } from "@/types/nav"
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
+
+import ArtistsNavMenu from "./nav/artists-menu"
+import EventNavMenu from "./nav/events-menu"
+import { NavLinkItem, NavMenuItem } from "./nav/nav-link"
+import { NavigationMenu, NavigationMenuList } from "./ui/navigation-menu"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -13,26 +14,26 @@ interface MainNavProps {
 export function ListLinks({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      
-      {items?.length ? (
-        <nav className="flex gap-6">
-          {items?.map(
-            (item, index) =>
-              item.href && (
-                <Link
-                  key={index}
-                  href={item.href}
-                  className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",item.customCSS,
-                    item.disabled && "cursor-not-allowed opacity-80",
-                  )}
-                >
-                  {item.title}
-                </Link>
-              )
-          )}
-        </nav>
-      ) : null}
+      <nav className="flex gap-6">
+        {/* all nav item should be under NavigationMenuList component */}
+        <NavigationMenu>
+          <NavigationMenuList>
+            {/* NavLinkItem for standaalone links */}
+            <NavLinkItem title="Home" url="/" />
+
+            {/* NavMenuItem for links with menu */}
+            <NavMenuItem title="Event">
+              <EventNavMenu />
+            </NavMenuItem>
+
+            <NavMenuItem title="Artists">
+              <ArtistsNavMenu />
+            </NavMenuItem>
+
+            <NavLinkItem title="About" url="/" />
+          </NavigationMenuList>
+        </NavigationMenu>
+      </nav>
     </div>
   )
 }
