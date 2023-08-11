1e54e23c3f83ac1efd5732c8f69468bd5689ebb7
danielmachado99 <danielmachado1999@gmail.com>
2023-08-11 11:51:35 +0530

    signin/signup buttons hidden when email logged in

--- tixspot/src/components/nav/right-nav.tsx
+++ tixspot/src/components/nav/right-nav.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link"
+import { getUserData } from "@/redux/features/authSlice"
+import { useSelector } from "react-redux"
 
 import { NavItem } from "@/types/nav"
 import { cn } from "@/lib/utils"
@@ -8,25 +10,30 @@ interface MainNavProps {
 }
 
 export function RightNav({ items }: MainNavProps) {
+  const userData = useSelector(getUserData)
   return (
     <div className="flex gap-6 md:gap-10">
       {items?.length ? (
         <nav className="flex gap-6">
-          {items?.map(
-            (item, index) =>
-              item.href && (
-                <Link
-                  key={index}
-                  href={item.href}
-                  className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",
-                    item.customCSS,
-                    item.disabled && "cursor-not-allowed opacity-80"
-                  )}
-                >
-                  {item.title}
-                </Link>
-              )
+          {userData.isAuthenticated ? (
+            <>Signed in</>
+          ) : (
+            items?.map(
+              (item, index) =>
+                item.href && (
+                  <Link
+                    key={index}
+                    href={item.href}
+                    className={cn(
+                      "flex items-center text-sm font-medium text-muted-foreground",
+                      item.customCSS,
+                      item.disabled && "cursor-not-allowed opacity-80"
+                    )}
+                  >
+                    {item.title}
+                  </Link>
+                )
+            )
           )}
         </nav>
       ) : null}
