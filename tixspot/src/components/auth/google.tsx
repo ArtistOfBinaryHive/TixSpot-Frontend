3df8b5d686f3fd3f1a0e0c44975b5a7b823b9ba1
danielmachado99 <danielmachado1999@gmail.com>
2023-08-11 09:58:24 +0530

    cleanup

--- tixspot/src/components/auth/google.tsx
+++ tixspot/src/components/auth/google.tsx
@@ -5,11 +5,14 @@ import {
   useGoogleLogin,
 } from "@react-oauth/google"
 
-import { Icons } from "@/components/icons"
+import { googleAuth } from "../api-calls/google-auth"
 
-import { Button } from "../ui/button"
+async function sendToBackend(token: string) {
+  const response = await googleAuth(token)
+  console.log(response)
+}
 
-function LoginWithGoogle({ isLoading, setIsLoading }: any) {
+function ContinueWithGoogle({ isLoading, setIsLoading }: any) {
   console.log(isLoading)
   return (
     <GoogleOAuthProvider clientId="249910114863-58vtiqb90mcm87h5vopi0b3c9v9nhfgl.apps.googleusercontent.com">
@@ -17,6 +20,7 @@ function LoginWithGoogle({ isLoading, setIsLoading }: any) {
         <GoogleLogin
           onSuccess={(credentialResponse: any) => {
             console.log(credentialResponse)
+            sendToBackend(credentialResponse.credential)
           }}
           onError={() => {
             console.log("Login Failed")
@@ -26,22 +30,10 @@ function LoginWithGoogle({ isLoading, setIsLoading }: any) {
           shape="rectangular"
           width={1}
           logo_alignment="center"
+          text="continue_with"
         />
-        {/* <p>a </p> */}
       </div>
     </GoogleOAuthProvider>
   )
 }
-function Login({ setIsLoading }: any) {
-  // const login = useGoogleLogin({
-  //   onSuccess: (tokenResponse) => {
-  //     console.log(tokenResponse)
-  //     setIsLoading(setIsLoading(false))
-  //     //send token to backend to verify it
-  //   },
-  //   flow: "auth-code",
-  // })
-  return <div></div>
-}
-
-export default LoginWithGoogle
+export default ContinueWithGoogle
